59372cd1e66b336a535959ba767e6f331347db51
pho <phofin@gmail.com>
2012-10-28 11:33:02 +0100

    Beginning LEM1802 implementation

--- devices.py
+++ devices.py
@@ -1,4 +1,5 @@
 
+
 class Device:
 
 	def __init__(self, name, id_low, id_high, version, manuf_low, manuf_high):
@@ -20,7 +21,52 @@ class LEM1802(Device):
 
 	def __init__(self):
 		super(LEM1802, self).__init__("LEM1802", 0x7349, 0xf615, 0x1802, 0x1c6c, 0x8b36)
+		self.lastB = 0
+		self.ram = []
+		self.disconnected = True
+		for i in range(384):
+			self.ram.append(0x0) # CELLS?
 
 	def hwi(self, dcpu):
-		print("IM THE MONITOR LOL")
-		print("DCPU A:{} B:{}".format(hex(dcpu.A()), hex(dcpu.B())))
+		A = dcpu.A()
+
+		if A == 0: #MEM_MAP_SCREEN
+
+			B = dcpu.B()
+			if B == 0:
+				self.lastB = B
+				self.disconnected = True
+				return
+
+			elif self.lastB == 0:
+				self.disconnected = False
+				self.lastB = B
+				sleep(1)
+				#TODO  Other interrupts sent during this time
+				#      are still processed.
+
+			for i in range(len(self.ram)):
+				#self.ram[i]( dcpu.ram[B+i]() )
+				self.ram[i] = dcpu.ram[B+i]()
+
+		elif A == 1:
+			pass
+		elif A == 2:
+			pass
+		elif A == 3:
+			pass
+		elif A == 4:
+			pass
+		elif A == 5:
+			pass
+	
+
+	def decodeColor(self, color):
+		# ffffbbbbBccccccc
+		char = i & 0x007f
+		B = (i>>7) & 0x1
+		b = (i>>8) & 0xf
+		f = i>>12
+
+		return (char, B, b, f)
+
